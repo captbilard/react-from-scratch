6d8d174472fe823242f463fe9137145fe3376371
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-08-26 22:13:01 +0100

    Included ES7 React/Redux extension Formatted with prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from "react";
+import ReactDom from "react-dom";
 
-function Greeting(){
-  return <h3>This is how we roll, we ensure to always stand out from the crowd like stars</h3>
+function Greeting() {
+  return (
+    <div>
+      <h3 style={{ "color":"red"}}>
+        This is how we roll, we ensure to always stand out from the crowd like
+        stars
+      </h3>
+      <ul>
+        <li>Benshima</li>
+        <li>Rejoice</li>
+        <li>Destiny</li>
+      </ul>
+    </div>
+  );
 }
 
-ReactDom.render(<Greeting/>, document.getElementById('root'))
+ReactDom.render(<Greeting />, document.getElementById("root"));
