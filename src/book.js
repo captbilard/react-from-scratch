be0c9ef74d5efad5e1c0a566f011ac83725ee4aa
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-08-29 12:59:56 +0100

    Cleaned up the code by adding & making use of variables

--- src/book.js
+++ src/book.js
@@ -1,34 +1,20 @@
 import React from 'react'
 
+//variables 
+const title = "The seven Husbands of Evelyn Hugo: A Novel"
+const author = "Taylor Jenkins Reid"
+const img = "https://images-na.ssl-images-amazon.com/images/I/71WAr4M5NlS._AC_UL200_SR200,200_.jpg"
+
+
 const Book = () => {
   return (
     <article>
-      <Image />
-      <Title />
-      <Author />
+      <img src={img} style={{ marginTop: "0.8rem" }} alt="" />
+      <h3>{title}</h3>
+      <p style={{fontWeight:"bold", fontSize:"0.75rem"}}>{ author}</p>
     </article>
-  )
+  );
 }
 
-const Image = () => {
-    return (
-      <img
-        src="https://images-na.ssl-images-amazon.com/images/I/71WAr4M5NlS._AC_UL200_SR200,200_.jpg"
-        alt="" style={{marginTop:"0.8rem"}}
-      />
-    );
-}
-
-const title = "The seven Husbands of Evelyn Hugo: A Novel"
-
-const Title = () => {
-  return <h3>{title}</h3>;
-}
-
-const author = "Taylor Jenkins Reid"
-
-const Author = () => {
-  return <p style={{ fontWeight: "bold", fontSize: "0.75rem" }}>{ author.toUpperCase()}</p>;
-}
 
 export default Book
