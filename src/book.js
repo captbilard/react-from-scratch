2228328309466f47c75a1f3199948582ba969c4d
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-08-29 13:44:39 +0100

    Using object destructuring

--- src/book.js
+++ src/book.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Book = (props) => {
+  //Using object destructuring
+  const {img,title,author} = props
   return (
     <article>
-      <img src={props.img} style={{ marginTop: "0.8rem" }} alt="" />
-      <h3>{props.title}</h3>
-      <p style={{ fontWeight: "bold", fontSize: "0.75rem" }}>{props.author}</p>
+      <img src={img} style={{ marginTop: "0.8rem" }} alt="" />
+      <h3>{title}</h3>
+      <p style={{ fontWeight: "bold", fontSize: "0.75rem" }}>{author}</p>
     </article>
   );
 };
--- src/index.js
+++ src/index.js
@@ -5,16 +5,33 @@ import Book from "./book";
 //CSS
 import "./index.css";
 
-//Variables
-const bookTitle = "The seven Husbands of Evelyn Hugo: A Novel";
-const bookAuthor = "Taylor Jenkins Reid";
-const bookImg =
-  "https://images-na.ssl-images-amazon.com/images/I/71WAr4M5NlS._AC_UL200_SR200,200_.jpg";
+//Data Objects
+const firstBook = {
+  title: "The seven Husbands of Evelyn Hugo: A Novel",
+  author: "Taylor Jenkins Reid",
+  img: "https://images-na.ssl-images-amazon.com/images/I/71WAr4M5NlS._AC_UL200_SR200,200_.jpg",
+};
+
+const secondBook = {
+  title:"The 48 Laws of Power",
+  img: "https://images-na.ssl-images-amazon.com/images/I/71951W96oWL._AC_UL200_SR200,200_.jpg",
+  author : "Robert Greene"
+}
+
 
 function BookList() {
   return (
     <section>
-      <Book title={bookTitle} author={bookAuthor} img={bookImg} />
+      <Book
+        title={firstBook.title}
+        author={firstBook.author}
+        img={firstBook.img}
+      />
+      <Book
+        title={secondBook.title}
+        author={secondBook.author}
+        img={secondBook.img}
+      />
     </section>
   );
 }
