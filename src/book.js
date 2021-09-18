750a20139492f0a8c3b4ce6f50475093b00d11e0
Adebayo Osinulu <bilardosinulu@gmail.com>
2021-09-18 19:17:00 +0100

    Wrote events handlers, and used them on certain event calls

--- src/book.js
+++ src/book.js
@@ -1,14 +1,28 @@
 import React from "react";
 
+//events, eventhandler
+//onDoubleClick, onMouseEnter
+
+
+const doubleClick = (title) => {
+  console.log(title);
+}
+
+const mouseEnter = (title, author) => {
+  console.log(`Title is ${title}, Author is ${author}`);
+}
+
 const Book = (props) => {
   //Using object destructuring
   console.log(props);
   const {img,title,author} = props.book
   return (
-    <article>
+    <article onMouseEnter={()=>{mouseEnter(title, author)}}>
       <img src={img} style={{ marginTop: "0.8rem" }} alt="" />
       <h3>{title}</h3>
       <p style={{ fontWeight: "bold", fontSize: "0.75rem" }}>{author}</p>
+      <button type="button" onDoubleClick={()=>doubleClick(title)}>Double Click Me</button>
+
     </article>
   );
 };
